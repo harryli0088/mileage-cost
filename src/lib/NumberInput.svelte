7c560c7e27feb455c1933e297a427f5229721dd3
harryli0088 <harryli0088@gmail.com>
2022-01-02 13:29:56 -0500

    draft

---
+++ src/lib/NumberInput.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+  import Fa from 'svelte-fa'
+  import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons'
+
+  export let id:string = ""
+  export let label:string = ""
+	export let value:number = 0
+  export let step:number = 1
+</script>
+
+<span>
+  <label for={id}>{label}</label>
+  <div class="number-input">
+    <button class="icon-container left" on:click={() => value = value - step}>
+      <Fa icon={faMinus}/>
+    </button>
+
+    <input
+      bind:value={value}
+      {id}
+      {step}
+      type="number"
+    />
+
+    <button class="icon-container right" on:click={() => value = value + step}>
+      <Fa icon={faPlus}/>
+    </button>
+  </div>
+</span>
+
+<style>
+  span {
+    display: inline-block;
+  }
+
+  input {
+    font-size: 1.25em;
+    width: 4em;
+    margin-bottom: 0;
+    text-align: center;
+  }
+
+  label {
+    font-weight: bold;
+  }
+
+  .number-input {
+    display: flex;
+    align-items: center;
+  }
+
+  .number-input button.icon-container {
+    font-size: 1.3em;
+    cursor: pointer;
+    transition: 0.25s;
+    margin-bottom: 0;
+    padding-left: 0.5em;
+    padding-right: 0.5em;
+    background-color: #ddd;
+  }
+  .number-input button.icon-container:hover {
+    transform: scale(1.1);
+  }
+  .number-input button.icon-container:active {
+    transform: scale(1);
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,35 @@
 <script lang="ts">
   import Fa from 'svelte-fa'
+  import { faBolt, faBurn } from '@fortawesome/free-solid-svg-icons'
   import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
   import Blanchor from '$lib/Blanchor.svelte';
-  // import { faFilePdf } from '@fortawesome/free-solid-svg-icons'
+import NumberInput from '$lib/NumberInput.svelte';
+
+  const ALL_UNITS = {
+    imperial: {
+      distance: "Miles",
+      volume: "Gallon",
+    },
+    metric: {
+      distance: "Kilometers",
+      volume: "Liter",
+    },
+  }
+
+  let costPerKwh:number = 0.23
+  let gasCostPerVolume:number = 3.5 //ie price of gas per gallon
+  let gasDistancePerVolume: number = 30 //ie mpg
+  let kwhPer100Distance:number = 27 // Tesla Model Y Long Range https://www.fueleconomy.gov/feg/Find.do?action=sbs&id=43406
+  let unitType: "imperial" | "metric" = "imperial"
+
+  $: electricCost = kwhPer100Distance * costPerKwh //kwh / 100 distance * cost / kwh = cost / 100 distance
+  $: gasCost = 100 * gasCostPerVolume / gasDistancePerVolume //100 distance * (cost/volume) / ( distance/volume ) = cost / 100 distance
+  $: units = unitType==="imperial" ? ALL_UNITS.imperial : ALL_UNITS.metric
 </script>
 
 <main>
-	<header>
+	<!-- <header>
     <div>
       <h1>Svelte Kit Template</h1>
       <br/>
@@ -17,21 +39,78 @@
         </Blanchor>
       </div>
     </div>
-  </header>
+  </header> -->
 
-  <section>
-    <p>This is my own Svelte Kit template! It includes:</p>
+  <div id="gas-container">
+    <h2>Gas Car <Fa icon={faBurn} style="color:red"/></h2>
+  
+    <div class="inputs-container">
+      <div>
+        <NumberInput
+          bind:value={gasDistancePerVolume}
+          id="miles-per-volume"
+          label={`Miles per ${units.volume}`}
+          step={1}
+        />
+      </div>
 
-    <ul>
-      <li>Fontawesome icons for Svelte</li>
-      <li>GitHub pages set up</li>
-      <li>Container styling</li>
-      <li>Some header/section/footer styling</li>
-    </ul>
+      <div>
+        <NumberInput
+          bind:value={gasCostPerVolume}
+          id="cost-per-volume"
+          label={`Cost per ${units.volume}`}
+          step={.1}
+        />
+      </div>
+    </div>
+
+    <br/>
+
+    <div>
+      <div><b>Cost per 100 {units.distance}</b></div>
+      <div>100 * {gasCostPerVolume} / {gasDistancePerVolume} = </div>
+      <br/>
+      <div class="cost">${gasCost.toFixed(2)}</div>
+    </div>
+  </div>
+
+  <div id="electric-container">
+    <h2>Electric Car <Fa icon={faBolt} style="color:gold"/></h2>
+
+    <div class="inputs-container">
+      <div>
+        <NumberInput
+          bind:value={costPerKwh}
+          id="cost-per-kwh"
+          label={`Cost per KWH`}
+          step={.01}
+        />
+      </div>
+      
+      <div>
+        <NumberInput
+          bind:value={kwhPer100Distance}
+          id="kwh-per-distance"
+          label={`KWH per 100 ${units.distance}`}
+          step={1}
+        />
+      </div>
+    </div>
+    
+
+    <br/>
+
+    <div>
+      <div><b>Cost per 100 {units.distance}</b></div>
+      <div>{kwhPer100Distance} * {costPerKwh} = </div>
+      <br/>
+      <div class="cost">${electricCost.toFixed(2)}</div>
+    </div>
+  </div>
 </main>
 
 <style>
-	header {
+	/* header {
 		display: flex;
 		align-items: center;
 		justify-content: center;
@@ -45,5 +124,84 @@
 		header {
 			height: 50vh;
 		}
-	}
+	} */
+
+  main {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  #gas-container, #electric-container {
+    width: 100%;
+    padding: 1em;
+    box-sizing: border-box;
+    text-align: center;
+  }
+
+  #gas-container {
+    background-color: #FDEDEC;
+    border-bottom: 1px solid gray;
+  }
+
+  #electric-container {
+    background-color: #EAFAF1;
+  }
+
+  .inputs-container > div {
+    display: inline-block;
+  }
+  .inputs-container > div:first-child {
+    padding-right: 0.25em;
+  }
+
+
+  /* @media only screen and (min-width: 600px) {
+  } */
+  @media only screen and (min-width: 1000px) {
+    main {
+      display: flex;
+      flex-direction: row;
+    }
+
+    #gas-container, #electric-container {
+      width: 50%;
+    }
+    
+    #gas-container {
+      border-right: 1px solid gray;
+      border-bottom: none;
+      text-align: right;
+    }
+
+    #electric-container {
+      text-align: left;
+    }
+
+    .inputs-container > div {
+      display: block;
+    }
+    .inputs-container > div:first-child {
+      padding-right: 0;
+      padding-bottom: 1em;
+    }
+  }
+
+  .cost {
+    font-size: 1.5em;
+    font-weight: bold;
+    display: inline-block;
+    padding-left: 0.5em;
+    padding-right: 0.5em;
+    padding-top: 0.25em;
+    padding-bottom: 0.25em;
+    color: white;
+  }
+  #gas-container .cost {
+    background-color: #E74C3C;
+  }
+  #electric-container .cost {
+    background-color: #2ECC71;
+  }
 </style>
